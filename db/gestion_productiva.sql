a30cd023136b049428951666d716193c7d08884d
lsantiago <lsquinones@gmail.com>
2017-05-04 00:24:30 -0500

    Actualizacion sript DB
    Script para generar base de datos (posee dos registros)

--- db/gestion_productiva.sql
+++ db/gestion_productiva.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2017 a las 02:15:55
+-- Tiempo de generación: 04-05-2017 a las 07:20:16
 -- Versión del servidor: 5.5.8
 -- Versión de PHP: 5.6.3
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestion_productiva`
 --
+CREATE DATABASE IF NOT EXISTS `gestion_productiva` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `gestion_productiva`;
 
 -- --------------------------------------------------------
 
@@ -38,14 +40,15 @@ CREATE TABLE IF NOT EXISTS `proyectos` (
   `perfil` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `nroEstudiantes` int(3) NOT NULL,
   `descripcionTrabajo` varchar(100) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `proyectos`
 --
 
 INSERT INTO `proyectos` (`id`, `nombreSolicitante`, `tipoOrganizacion`, `dependencia`, `telefono`, `email`, `tipoPractica`, `resultados`, `perfil`, `nroEstudiantes`, `descripcionTrabajo`) VALUES
-(14, 'Santiago QuiÃ±ones', 'Externa', 'DGMIC', '0988995308', 'lsquinones@gmail.com', 'PrÃ¡cticas pre profesionales en empresa', 'App Movil', 'Desarrollo de aplicaciones', 14, '');
+(21, 'Santiago QuiÃ±ones', 'Externa', 'DGMIC', '0988995308', 'lsquinones@gmail.com', 'ParticipaciÃ³n en proyectos UTPL', 'App Movil', 'Desarrollo de aplicaciones', 2, 'Desarrollo de aplicaciÃ³n mÃ³vil para el cÃ³digo de la construcciÃ³n INEC'),
+(22, 'Karla Romero', 'Vinculacion', 'UGTI', '0988995308', 'karomero@utpl.edu.ec', 'ParticipaciÃ³n en proyectos UTPL', 'Informe de auditoria - caso ILE', 'CapacitaciÃ³n a terceros (solo vinculaciÃ³n)', 3, 'Auditoria a empresa ILE2');
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +68,7 @@ ALTER TABLE `proyectos`
 -- AUTO_INCREMENT de la tabla `proyectos`
 --
 ALTER TABLE `proyectos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
